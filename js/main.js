3242da7d032c802e7d95865a2d43844cc9161c2b
vat9187 <vat9187@gmail.com>
2024-11-27 09:53:44 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+let reg_but = document.querySelector('#reg_but'),
+    text = document.querySelector('#wasd')
+   
+
+
 document.querySelector('#reg_show').addEventListener('click', ()=>{
    document.querySelector('#aut').style.display = 'none'
    document.querySelector('#reg').style.display = 'block'
@@ -5,4 +10,20 @@ document.querySelector('#reg_show').addEventListener('click', ()=>{
 document.querySelector('#aut_show').addEventListener('click', ()=>{
    document.querySelector('#reg').style.display = 'none'
    document.querySelector('#aut').style.display = 'block'
+})
+
+
+reg_but.addEventListener('click', ()=>{
+   
+   let reg_name = document.querySelector('#reg_name'),
+       reg_email = document.querySelector('#reg_email'),
+       reg_pass = document.querySelector('#reg_pass'),
+       asd = document.querySelector('#asd')
+       text.innerHTML =''
+   if(reg_name.value == ''|| reg_email.value == '' || reg_pass.value == ''  ){
+      text.innerHTML = 'пустые поля'
+   } 
+
+    
+
 })
